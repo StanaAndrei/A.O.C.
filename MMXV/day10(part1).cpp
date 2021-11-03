ed6b193ce6780ed814ef662d549577df7c0a6cb1
StanaAndrei <stadey33@gmail.com>
2021-11-03 11:41:09 +0200

    Create day10(part1).cpp

---
+++ MMXV/day10(part1).cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+constexpr char start[] = "1321131112";
+constexpr int n = 40;
+
+string lookAndSay(string s) {
+    string ans = "";
+    regex rgx(R"((\d)\1*)");
+    regex_iterator<string::iterator> it(s.begin(), s.end(), rgx), endIt;
+    for (; it != endIt; it++) {
+        const string &temp = it->str();
+        ans += to_string(temp.size()) + temp[0];
+    }
+    return ans;
+}
+
+signed main() {
+    string s = start;
+    for (int i = 0; i < n; i++) {
+        s = lookAndSay(s);
+    }
+    cout << s.size() << endl;
+    return 0;
+}
