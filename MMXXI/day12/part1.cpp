cc9a9386f18ef4b5d1d96af19fc0502ad0f74ec9
StanaAndrei <stadey33@gmail.com>
2021-12-12 11:58:50 +0200

    Create part1.cpp

---
+++ MMXXI/day12/part1.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+map<string, set<string>> edges;
+
+void mkGraph() {
+    for (string line; getline(cin, line);) {
+        stringstream ss(line);
+        string a, b;
+        for (string s; getline(ss, s, '-');) {
+            if (a.empty()) {
+                a = s;
+            } else {
+                b = s;
+            }
+        }
+        edges[a].insert(b);
+        edges[b].insert(a);
+    }
+}
+
+int dfs(string s, set<string> trav) {
+    if (s == "end") {
+        return 1;
+    }
+    if (islower(s[0])) {
+        trav.insert(s);
+    }
+    int r = 0;
+    for (const auto &nxt : edges[s]) {
+        if (!trav.count(nxt)) {
+            r += dfs(nxt, trav);
+        }
+    }
+    return r;
+}
+
+signed main() {
+    freopen("text.in", "r", stdin);
+    mkGraph();
+    const int ans = dfs("start", {});
+    cout << ans << endl;
+    return 0;
+}
